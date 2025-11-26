497cb3c44de24c99a6a8fa54d9950f9780c9c8be
chiedozie07 <ezidiegwuchiedozie17@gmail.com>
2025-11-26 16:42:45 +0100

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: __dirname, // Prevents Next.js from scanning protected macOS folders
+  },
 };
 
 export default nextConfig;
